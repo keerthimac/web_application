3a91caaf0cfee30d60e13763ba928ba8af5ff457
Darshana <keerthi.mac@gmail.com>
2022-12-11 09:37:04 +0530

    added codes to routs,controllers,middleware
    -error handle middleware created
    -added to server as a middleware
    
    -sub contractor controller created (read,create,update,delete)
    -sub contractor routs created
    -added these routs to the server
    -tested with postman & connection success
    
    -this for further prisma testing
    
    -plus
    Requirement doc created as a text
    these packages added
      *express-async-handler

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const colors = require("colors");
+const dontenv = require("dotenv").config();
 const PORT = process.env.PORT || 5000;
+const errorHandler = require("./middleware/errorMiddleware");
 
 const app = express();
 
@@ -8,8 +10,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "Welcome to Material database" });
+  res.status(200).json({ message: "Welcome to web-server" });
 });
+
+//Routes
+app.use("/api/subContractors", require("./routes/subContractorRoute"));
+
+//Middleware
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`.brightMagenta.underline);
