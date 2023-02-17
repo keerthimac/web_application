1d708dfe9ed9f07331c449178bef1d4f5fa8fd2b
Darshana <keerthi.mac@gmail.com>
2023-02-17 06:36:36 +0530

    server error resolved

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,6 @@ const colors = require("colors");
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 const multer = require("multer");
-const path = require("path");
 const errorHandler = require("./middleware/errorMiddleware");
 const cors = require("cors");
 
